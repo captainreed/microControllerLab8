e1be1494bffb3acfde73f90a8e4d8a723c2d689d
captainReed <fearlessleader2.0@gmail.com>
2018-11-01 14:07:17 -0600

    all files in place

---
+++ waveGenerator.h
@@ -0,0 +1,13 @@
+#ifndef WAVEGENERATOR_H
+#define WAVEGENERATOR_H
+
+#include <stdint.h>
+
+#define bool _Bool
+
+void configInterupts(void);
+void setFreq(int frequency);
+
+
+
+#endif /* WAVEGENERATOR_H */
