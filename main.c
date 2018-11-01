aa4da14e4e5ec18e75068ee3185b10c7d9eeeb04
captainreed <fearlessleader2.0@gmail.com>
2018-11-01 13:15:07 -0600

    pseudo code set up

--- main.c
+++ main.c
@@ -1,13 +1,40 @@
 #include "PiezoBuzzer.h"
 #include "stm32l476xx.h"
 
+//new files:
+//readADC.c
+//waveGenerator.c this is where the interrupts will be initiated
+
+
+//interupt 1
+/*
+set the interupt to be triggered every 2 ms
+read the value every trigger period and store it into memory
+*/
+
+//interupt 2
+/*
+configure to be triggered every 500 ms
+read the value from the ADC memory
+newFreq = math.floor(100 + 900*(adcValue/maxADCValue))
+call setFreq(newFreq)
+*/
+
+
+
 int main(void){
 	
 	initBuzzer();
-//<<<<<<< HEAD
+//pseudo code****************
+/*
+configure a timer interrupt to trigger the ADC read function and write to memory
+in wavegenerator.c write an array with 40 entries that are samples from a sin wave
 
-//>>>>>>> 4be629ff626c1b71e251d93e8ef57e82205fe34f
+configure another timer that triggers the setFreq() function in waveGenerator.c every 500ms
+the frequency can be between 100 and 1000HZ
+the setFreq(freq in HZ) function changes the time constant
 
+*/
 
 	while(1)
 	{
