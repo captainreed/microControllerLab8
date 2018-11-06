591ab6d242aa0b785f266e4ab016369297a3003d
BenjaminRSmith <44481305+BenjaminRSmith@users.noreply.github.com>
2018-11-06 09:35:14 -0700

    updated function names and built DACinit (still needs timer set up)

--- readDAC.c
+++ readDAC.c
@@ -1,11 +1,30 @@
-#include "readadc.h"
+#include "readDAC.h"
 
-void adcinit()
+void DACinit()
 {
-
+  RCC->APB1ENR1 |= RCC_APB1ENR1_DAC1EN;// turn on dac clock
+  DAC->CR&= ~( DAC_CR_EN1 | DAC_CR_EN2);// turn off dac so you can alter registers
+  DAC->MCR &= ~( DAC_MCR_MODE2_0 | DAC_MCR_MODE2_1 | DAC_MCR_MODE2_2 ); // clear channel 2 register
+  DAC->MCR |=  ~DAC_MCR_MODE2_0 | ~DAC_MCR_MODE2_1 | ~DAC_MCR_MODE2_2; // mode0 000 is external pin buffered 
+    //mode1 010 is external pin not buffered
+    //mode2 100 is external pin buffered with sample and hold
+    //mode3 110 is external pin not buffered with sample and hold
+    //book recommends mode0 i see potential for the other 3 modes and there are others beyond these 4
+  DAC->CR |= DAC_CR_TEN2; //enable trigger for channel 2
+  DAC->CR &= ~( DAC_CR_TSEL2_0 | DAC_CR_TSEL2_1 | DAC_CR_TSEL2_2);// clear trigger selection
+  DAC->CR |= DAC_CR_TSEL2_0;
+  DAC->CR |= DAC_CR_TSEL2_1; // select TIM5_TRGO
+  DAC->CR |= DAC_CR_EN2;
+  
+  RCC->AHB2ENR |= RCC_AHB2ENR_GPIOAEN;//enable port A clock
+  //set to analog mode pin A5
+  GPIOA-> MODER |= GPIO_MODER_MODER5_0;
+  GPIOA-> MODER |= GPIO_MODER_MODER5_1;
+  
+  
 }
 
-void readadc()
+void readDAC()
 {
 
 }
